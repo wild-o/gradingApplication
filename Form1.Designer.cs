08526cd0f3febd42d4d0eaa91f2b1eea120d1860
Albert Arias <47288705+wild-o@users.noreply.github.com>
2020-01-07 20:16:37 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace Gradingapplicationproject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblGradeapp = new System.Windows.Forms.Label();
+            this.txtProjectgrade = new System.Windows.Forms.TextBox();
+            this.lblProjectgrade = new System.Windows.Forms.Label();
+            this.txtAssignment1 = new System.Windows.Forms.TextBox();
+            this.txtAssignment2 = new System.Windows.Forms.TextBox();
+            this.txtAssignment3 = new System.Windows.Forms.TextBox();
+            this.txtAssignment4 = new System.Windows.Forms.TextBox();
+            this.txtExam1 = new System.Windows.Forms.TextBox();
+            this.txtExam2 = new System.Windows.Forms.TextBox();
+            this.txtAbscenses = new System.Windows.Forms.TextBox();
+            this.txtFinalgrade = new System.Windows.Forms.TextBox();
+            this.txtLettergrade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblGradeapp
+            // 
+            this.lblGradeapp.AutoSize = true;
+            this.lblGradeapp.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGradeapp.Location = new System.Drawing.Point(127, 9);
+            this.lblGradeapp.Name = "lblGradeapp";
+            this.lblGradeapp.Size = new System.Drawing.Size(261, 55);
+            this.lblGradeapp.TabIndex = 0;
+            this.lblGradeapp.Text = "Grade app";
+            // 
+            // txtProjectgrade
+            // 
+            this.txtProjectgrade.Location = new System.Drawing.Point(137, 81);
+            this.txtProjectgrade.Name = "txtProjectgrade";
+            this.txtProjectgrade.Size = new System.Drawing.Size(100, 20);
+            this.txtProjectgrade.TabIndex = 1;
+            // 
+            // lblProjectgrade
+            // 
+            this.lblProjectgrade.AutoSize = true;
+            this.lblProjectgrade.Location = new System.Drawing.Point(48, 88);
+            this.lblProjectgrade.Name = "lblProjectgrade";
+            this.lblProjectgrade.Size = new System.Drawing.Size(70, 13);
+            this.lblProjectgrade.TabIndex = 2;
+            this.lblProjectgrade.Text = "Project grade";
+            // 
+            // txtAssignment1
+            // 
+            this.txtAssignment1.Location = new System.Drawing.Point(137, 119);
+            this.txtAssignment1.Name = "txtAssignment1";
+            this.txtAssignment1.Size = new System.Drawing.Size(100, 20);
+            this.txtAssignment1.TabIndex = 1;
+            this.txtAssignment1.TextChanged += new System.EventHandler(this.txtAssignment1_TextChanged);
+            // 
+            // txtAssignment2
+            // 
+            this.txtAssignment2.Location = new System.Drawing.Point(137, 145);
+            this.txtAssignment2.Name = "txtAssignment2";
+            this.txtAssignment2.Size = new System.Drawing.Size(100, 20);
+            this.txtAssignment2.TabIndex = 1;
+            // 
+            // txtAssignment3
+            // 
+            this.txtAssignment3.Location = new System.Drawing.Point(137, 171);
+            this.txtAssignment3.Name = "txtAssignment3";
+            this.txtAssignment3.Size = new System.Drawing.Size(100, 20);
+            this.txtAssignment3.TabIndex = 1;
+            // 
+            // txtAssignment4
+            // 
+            this.txtAssignment4.Location = new System.Drawing.Point(137, 197);
+            this.txtAssignment4.Name = "txtAssignment4";
+            this.txtAssignment4.Size = new System.Drawing.Size(100, 20);
+            this.txtAssignment4.TabIndex = 1;
+            // 
+            // txtExam1
+            // 
+            this.txtExam1.Location = new System.Drawing.Point(137, 243);
+            this.txtExam1.Name = "txtExam1";
+            this.txtExam1.Size = new System.Drawing.Size(100, 20);
+            this.txtExam1.TabIndex = 1;
+            // 
+            // txtExam2
+            // 
+            this.txtExam2.Location = new System.Drawing.Point(137, 269);
+            this.txtExam2.Name = "txtExam2";
+            this.txtExam2.Size = new System.Drawing.Size(100, 20);
+            this.txtExam2.TabIndex = 1;
+            // 
+            // txtAbscenses
+            // 
+            this.txtAbscenses.Location = new System.Drawing.Point(137, 306);
+            this.txtAbscenses.Name = "txtAbscenses";
+            this.txtAbscenses.Size = new System.Drawing.Size(100, 20);
+            this.txtAbscenses.TabIndex = 1;
+            // 
+            // txtFinalgrade
+            // 
+            this.txtFinalgrade.Location = new System.Drawing.Point(355, 352);
+            this.txtFinalgrade.Name = "txtFinalgrade";
+            this.txtFinalgrade.ReadOnly = true;
+            this.txtFinalgrade.Size = new System.Drawing.Size(100, 20);
+            this.txtFinalgrade.TabIndex = 1;
+            // 
+            // txtLettergrade
+            // 
+            this.txtLettergrade.Location = new System.Drawing.Point(355, 380);
+            this.txtLettergrade.Name = "txtLettergrade";
+            this.txtLettergrade.ReadOnly = true;
+            this.txtLettergrade.Size = new System.Drawing.Size(100, 20);
+            this.txtLettergrade.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 122);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Assignment 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(48, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Assignment 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Assignment 3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 204);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Assignment 4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(48, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Exam 1";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(48, 276);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Exam 2";
+            this.label6.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(48, 313);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "# Absences";
+            this.label7.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(266, 359);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(76, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Numeric grade";
+            this.label8.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(278, 387);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(64, 13);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Letter grade";
+            this.label9.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnCalculate.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnCalculate.Location = new System.Drawing.Point(100, 351);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(106, 29);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 412);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblProjectgrade);
+            this.Controls.Add(this.txtLettergrade);
+            this.Controls.Add(this.txtFinalgrade);
+            this.Controls.Add(this.txtAbscenses);
+            this.Controls.Add(this.txtExam2);
+            this.Controls.Add(this.txtExam1);
+            this.Controls.Add(this.txtAssignment4);
+            this.Controls.Add(this.txtAssignment3);
+            this.Controls.Add(this.txtAssignment2);
+            this.Controls.Add(this.txtAssignment1);
+            this.Controls.Add(this.txtProjectgrade);
+            this.Controls.Add(this.lblGradeapp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGradeapp;
+        private System.Windows.Forms.TextBox txtProjectgrade;
+        private System.Windows.Forms.Label lblProjectgrade;
+        private System.Windows.Forms.TextBox txtAssignment1;
+        private System.Windows.Forms.TextBox txtAssignment2;
+        private System.Windows.Forms.TextBox txtAssignment3;
+        private System.Windows.Forms.TextBox txtAssignment4;
+        private System.Windows.Forms.TextBox txtExam1;
+        private System.Windows.Forms.TextBox txtExam2;
+        private System.Windows.Forms.TextBox txtAbscenses;
+        private System.Windows.Forms.TextBox txtFinalgrade;
+        private System.Windows.Forms.TextBox txtLettergrade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gradingapplicationproject
+{
+    public partial class Form1 : Form
+    {
+        int absencePoints = 0;
+        string letterGrade = "";
+        int projgrade = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (txtProjectgrade.Text.Trim() == "")
+            {
+                lblProjectgrade.ForeColor = Color.Red;
+            }
+            else
+            {
+                projgrade = Convert.ToInt16(txtProjectgrade.Text);
+            }
+
+
+            int assign1 = Convert.ToInt16(txtAssignment1.Text);
+            int assign2 = Convert.ToInt16(txtAssignment2.Text);
+            int assign3 = Convert.ToInt16(txtAssignment3.Text);
+            int assign4 = Convert.ToInt16(txtAssignment4.Text);
+            int exam1 = Convert.ToInt16(txtExam1.Text);
+            int exam2 = Convert.ToInt16(txtExam2.Text);
+            int absence = Convert.ToInt16(txtAbscenses.Text);
+
+            if (absence <= 2)
+            {
+                absencePoints = 10;
+            }
+            else if (absence == 3)
+            {
+                absencePoints = 5;
+            }
+
+            double numGrade = (projgrade * .2) + (assign1 * .1) + (assign2 * .1) + (assign3 * .1) + (assign4 * .1) + (exam1 * .15) + (exam2 * .15) + absencePoints;
+            txtFinalgrade.Text = numGrade.ToString();
+
+            if (numGrade >= 90 && numGrade <= 100)
+            {
+                letterGrade = "A";
+            }
+            else if (numGrade >= 80 && numGrade < 90)
+            {
+                letterGrade = "B";
+            }
+            else if (numGrade >= 70 && numGrade < 80)
+            {
+                letterGrade = "C";
+            }
+            else if (numGrade >= 60 && numGrade < 70)
+            {
+                letterGrade = "D";
+            }
+            else if (numGrade < 60)
+            {
+                letterGrade = "You need help.";
+            }
+            txtLettergrade.Text = letterGrade;
+        }
+
+        private void txtAssignment1_TextChanged(object sender, EventArgs e)
+        {
+            if (txtAssignment1.Text == String.Empty)
+            {
+                txtAssignment1.Focus();
+                txtAssignment1.BackColor = Color.Red;
+            }
+            else if (txtAssignment2.Text == String.Empty)
+            {
+                txtAssignment2.Focus();
+                txtAssignment2.BackColor = Color.Red;
+
+            }
+            else if (txtAssignment3.Text == String.Empty)
+            {
+                txtAssignment3.Focus();
+                txtAssignment3.BackColor = Color.Red;
+
+
+            }
+            else if (txtAssignment4.Text == String.Empty)
+            {
+                txtAssignment4.Focus();
+                txtAssignment4.BackColor = Color.Red;
+
+            }
+            else if (txtExam1.Text == String.Empty)
+            {
+                txtExam1.Focus();
+                txtExam1.BackColor = Color.Red;
+            }
+            else if (txtExam2.Text == String.Empty)
+            {
+                txtExam2.Focus();
+                txtExam2.BackColor = Color.Red;
+            }
+            else if (txtAbscenses.Text == String.Empty)
+            {
+                txtAbscenses.Focus();
+                txtAbscenses.BackColor = Color.Red;
+            }
+            else if (txtFinalgrade.Text == String.Empty)
+            {
+                txtFinalgrade.Focus();
+                txtFinalgrade.BackColor = Color.Red;
+            }
+            else if (txtLettergrade.Text == String.Empty)
+            {
+                txtLettergrade.Focus();
+                txtLettergrade.BackColor = Color.Red;
+            }
+
+
+
+
+
+
+        }
+    }
+}
